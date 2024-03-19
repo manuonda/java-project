afe4f76f1734f6c6ad161c3555e10c01cb16dd0e
David Garcia <david.garcia@grupomost.com>
2024-03-19 20:00:33 -0300

    Add Project Testing webflux

---
+++ testing-webflux/src/main/java/com/testing/webflux/TestingWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.testing.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingWebFluxApplication.class, args);
+	}
+
+}
