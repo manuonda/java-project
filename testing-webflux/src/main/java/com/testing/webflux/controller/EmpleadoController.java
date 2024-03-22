acf1c40e3fdb110d57e0aba71a37c0bf1d2a0056
David Garcia <david.garcia@grupomost.com>
2024-03-22 20:40:44 -0300

    Add Controller y servicio de Implementacion

--- testing-webflux/src/main/java/com/testing/webflux/controller/EmpleadoController.java
+++ testing-webflux/src/main/java/com/testing/webflux/controller/EmpleadoController.java
@@ -3,17 +3,19 @@ package com.testing.webflux.controller;
 
 import com.testing.webflux.dto.EmpleadoDTO;
 import com.testing.webflux.service.EmpleadoService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/empleados")
+@AllArgsConstructor
 public class EmpleadoController {
 
 
-  @Autowired
   private  EmpleadoService empleadoService;
 
 
@@ -22,4 +24,30 @@ public class EmpleadoController {
   public Mono<EmpleadoDTO> guardarEmpleado(@RequestBody EmpleadoDTO empleadoDTO){
     return empleadoService.guardarEmpleado(empleadoDTO);
   }
+
+
+  @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Mono<EmpleadoDTO> findById(@PathVariable("id") String idEmpleado){
+    return empleadoService.buscarPorId(idEmpleado);
+  }
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public Flux<EmpleadoDTO> findAll(){
+    return this.empleadoService.getAllEmpleados();
+  }
+
+
+  @PutMapping("{id}")
+  public Mono<EmpleadoDTO> update(@PathVariable("id") String idEmpleado, @RequestBody EmpleadoDTO dto){
+    return this.empleadoService.actualizarEmpleado(dto, idEmpleado);
+  }
+
+  @DeleteMapping("{id}")
+  public Mono<Void> delete(@PathVariable("id") String idEmpleado){
+    return this.empleadoService.eliminarEmpleado(idEmpleado);
+  }
+
+
 }
--- testing-webflux/src/main/java/com/testing/webflux/repository/EmpleadoRepository.java
+++ testing-webflux/src/main/java/com/testing/webflux/repository/EmpleadoRepository.java
@@ -3,6 +3,6 @@ package com.testing.webflux.repository;
 import com.testing.webflux.entity.Empleado;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface EmpleadoRepository extends ReactiveCrudRepository<Empleado, Long> {
+public interface EmpleadoRepository extends ReactiveCrudRepository<Empleado, String> {
 
 }
