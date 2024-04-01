a2f600da1ba9c6091d6a8c81e2dfd716db045086
manuonda <manuonda@gmail.com>
2024-04-01 13:10:23 -0300

    init project ia chat example

---
+++ ia/open-ai-chat/src/main/java/com/openai/chat/OpenAiChatApplication.java
@@ -0,0 +1,13 @@
+package com.openai.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAiChatApplication.class, args);
+	}
+
+}
