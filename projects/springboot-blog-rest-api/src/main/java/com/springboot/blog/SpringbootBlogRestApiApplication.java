2239cee8da210c93b03d32dd625f86c82cf4d567
david garcia <david.garcia@grupomost.com>
2024-04-09 07:35:16 -0300

    Configure project docker compose mysql

--- projects/springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ projects/springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,13 +1,21 @@
 package com.springboot.blog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		var informacion  =" Prueba";
+		System.out.println("Informacion numeo 23");
 	}
 
 }
