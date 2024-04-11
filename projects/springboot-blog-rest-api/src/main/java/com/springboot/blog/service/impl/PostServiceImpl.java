9b976bdfbb1e301b4ac1d7f5052a75f9ad4f1471
david garcia <david.garcia@grupomost.com>
2024-04-11 15:45:24 -0300

    Refactor service implement Post

--- projects/springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ projects/springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -2,6 +2,7 @@ package com.springboot.blog.service.impl;
 
 import com.springboot.blog.entity.Post;
 import com.springboot.blog.exception.EntityFoundException;
+import com.springboot.blog.exception.EntityNotFoundException;
 import com.springboot.blog.payload.PostDTO;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
@@ -28,33 +29,78 @@ public class PostServiceImpl implements PostService {
           throw new EntityFoundException("The Post title exist");
         }
 
+        Post post = this.mapToEntity(postDTO);
+        this.postRepository.save(post);
+        return this.mapToDTO(post);
 
-
-
-        return null;
     }
 
     @Override
     public List<PostDTO> getAllPosts() {
-        return null;
+        List<Post> posts = this.postRepository.findAll();
+        return posts.stream()
+                .map(entity -> mapToDTO(entity))
+                .toList();
     }
 
     @Override
     public PostDTO updatePost(PostDTO postDTO, Long id) {
-        return null;
+
+        Post post = this.postRepository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException("Post not exist"));
+
+        // Check if title already exists in another post 
+        postRepository.findByTitle(postDTO.getTitle())
+                      .ifPresent(existPost -> {
+                         if( !(existPost instanceof Post && 
+                               existPost.getId().equals(id))) {
+                            throw new EntityFoundException("Exist the title in another post");
+                        }
+                      });
+        post.setTitle(postDTO.getTitle());
+        post.setContent(postDTO.getContent());
+        post.setDescription(postDTO.getDescription());
+
+        this.postRepository.save(post);
+        return this.mapToDTO(post);
+
     }
+                       
+
 
     @Override
     public PostDTO findById(Long id) {
-        return null;
+      return this.postRepository.findById(id)
+       .map(this::mapToDTO)
+       .orElseThrow(() -> new EntityNotFoundException("Post not exist"));
     }
 
     @Override
-    public void deleteById(Long id) {
-
+    public void deleteById(Long id) {        
+      if( !postRepository.existsById(id)){
+         throw new EntityNotFoundException("Post Not Found");
+      }
+      this.postRepository.deleteById(id);
     }
 
 
 
+    public Post mapToEntity(PostDTO postDTO){
+        Post post = new Post();
+        post.setId(postDTO.getId());
+        post.setTitle(postDTO.getTitle());
+        post.setDescription(postDTO.getDescription());
+        post.setContent(postDTO.getContent());
+        return post;
+    }
+
+    public PostDTO mapToDTO(Post post){
+        PostDTO dto = new PostDTO();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setContent(post.getContent());
+        dto.setDescription(post.getDescription());
+        return dto;
+    }
 
 }
