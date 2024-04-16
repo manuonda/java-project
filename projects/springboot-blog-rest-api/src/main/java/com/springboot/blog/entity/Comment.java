5a4797f183bdad141e8e54b6123e00c12c093e37
David Garcia <david.garcia@grupomost.com>
2024-04-16 20:51:34 -0300

    Add init test controller Post

---
+++ projects/springboot-blog-rest-api/src/main/java/com/springboot/blog/entity/Comment.java
@@ -0,0 +1,18 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="")
+public class Comment {
+
+}
--- projects/springboot-blog-rest-api/src/test/java/com/springboot/blog/controller/PostControllerTests.java
+++ projects/springboot-blog-rest-api/src/test/java/com/springboot/blog/controller/PostControllerTests.java
@@ -1,17 +1,74 @@
 package com.springboot.blog.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.hamcrest.CoreMatchers.is;
+
+
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.payload.PostDTO;
+import com.springboot.blog.service.PostService;
 import com.springboot.blog.service.impl.PostServiceImpl;
 
 @WebMvcTest(PostController.class)
-public class PostControllerTests {
+public class PostControllerTests  {
 
 
-    @Mock
-    private PostServiceImpl   postServiceImpl;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+
+    @Test
+    @DisplayName("Test Junit Method Save Post")
+    public void givenObjectUsuarioDTO_whenSavePost_thenReturnObjectPostDTO() throws JsonProcessingException, Exception{
+        //given
+        PostDTO dto  = new PostDTO();
+        dto.setTitle("title one");
+        dto.setContent("content one");
+        dto.setDescription("description one");
+        
+        BDDMockito.given(this.postService.createPost(any(PostDTO.class)))
+        .willAnswer((invocation) -> invocation.getArgument(0));
+
+        //when 
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/posts")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(dto)));
+        
+        //then
+        resultActions.andExpect(status().isOk())
+        .andDo(print())
+        .andExpect(jsonPath("$.title", is("title one")))
+        .andExpect(jsonPath("$.content", is("content one")));
+    }
+    
+
 
 
 }
