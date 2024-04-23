68580230dc2da36d8b815c0c62950a905517fbb0
David Garcia <david.garcia@grupomost.com>
2024-04-23 20:39:43 -0300

    Add entitys User and Role

---
+++ projects/springboot-blog-rest-api/src/main/java/com/springboot/blog/entity/User.java
@@ -0,0 +1,41 @@
+package com.springboot.blog.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private Email email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable( name="user_roles",
+       joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+       inverseJoinColumns = @JoinColumn(name="role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
+
+}
