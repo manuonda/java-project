a0c084c12d254c4fc63cd6e641227a0a6c6e6fcb
david garcia <david.garcia@grupomost.com>
2024-05-02 14:58:42 -0300

    Add project base rag documentos

---
+++ ia/tutorial-base-rag/src/main/java/com/tutorial/base/rag/TutorialProjectRagApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.base.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialProjectRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialProjectRagApplication.class, args);
+	}
+
+}
