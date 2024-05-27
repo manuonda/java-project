f9027b02b3152c5dad71de4c86753993c7391773
david garcia <david.garcia@grupomost.com>
2024-05-27 14:40:51 -0300

    Test Category

--- projects/springboot-blog-rest-api/src/test/java/com/springboot/blog/repository/CategoryTest.java
+++ projects/springboot-blog-rest-api/src/test/java/com/springboot/blog/repository/CategoryTest.java
@@ -1,7 +1,12 @@
 package com.springboot.blog.repository;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.springboot.blog.entity.Category;
 
 
 @DataJpaTest
@@ -11,6 +16,26 @@ public class CategoryTest {
     private CategoryRepository categoryRepository;
 
 
+    Category category;
+
+    @BeforeEach
+    public void setUp(){
+      category = Category.builder()
+      .name("Category number1")
+      .description("Descriptino Category number 1")
+      .build();
+    }
+
+
+    @Test
+    @DisplayName("Test Method Save Category")
+    public void givenObjectCategory_whenSaveCategory_thenReturnObjectCategory(){
+      //given
+      Cageg 
+      //when
+      //then
+
+    }
     
 
 
