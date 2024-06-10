d3a6d8ca4e3370c8e464a4b056e6bf465980034d
David Garcia <david.garcia@grupomost.com>
2024-06-10 20:59:59 -0300

    Add @Configuration and @Bean

--- projects/spring-annotation-example/src/main/java/com/spring/annotations/example/DescriptionSpringBootAnnotationApplication.java
+++ projects/spring-annotation-example/src/main/java/com/spring/annotations/example/DescriptionSpringBootAnnotationApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.spring.annotations.example.controllers.PizzaController;
+import com.spring.annotations.example.services.VegPizza;
 
 @SpringBootApplication
 public class DescriptionSpringBootAnnotationApplication {
@@ -12,6 +13,12 @@ public class DescriptionSpringBootAnnotationApplication {
 		var context = SpringApplication.run(DescriptionSpringBootAnnotationApplication.class, args);
 	    var pizzaController =(PizzaController) context.getBean("pizzaController");
 		System.out.println(pizzaController.getPizza());
+
+		VegPizza vegPizza =  context.getBean(VegPizza.class);
+		System.out.println(vegPizza.getPizza());
+
+		VegPizza vegPizza2 = (VegPizza) context.getBean("vegPizzaBean");
+		System.out.println(vegPizza2.getPizza());
 	}
 
 }
