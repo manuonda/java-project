c1da40157011a25514b34dc8fae3116279a513bd
David Garcia <david.garcia@grupomost.com>
2024-07-16 20:50:42 -0300

    Add files configuration records, to notification service

--- projects/spring-boot-msvc-bookstore/notification-service/src/test/java/com/dgarcia/bookstore/notifications/TestNotificationServiceApplication.java
+++ projects/spring-boot-msvc-bookstore/notification-service/src/test/java/com/dgarcia/bookstore/notifications/TestNotificationServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestNotificationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(NotificationServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(NotificationServiceApplication::main).with(ContainersConfig.class).run(args);
 	}
 
 }
