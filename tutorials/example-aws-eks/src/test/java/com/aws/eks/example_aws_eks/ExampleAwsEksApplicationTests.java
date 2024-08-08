023717612e044df579ae88aeb45628be462a1095
david garcia <david.garcia@grupomost.com>
2024-08-08 15:46:27 -0300

    Init project aws eks example

---
+++ tutorials/example-aws-eks/src/test/java/com/aws/eks/example_aws_eks/ExampleAwsEksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.eks.example_aws_eks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleAwsEksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
