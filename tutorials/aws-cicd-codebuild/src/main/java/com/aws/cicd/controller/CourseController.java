8b66b34849ca73e747e9200f03881f9cc91c53d1
david garcia <david.garcia@grupomost.com>
2024-08-20 22:09:55 -0300

    fake commit

--- tutorials/aws-cicd-codebuild/src/main/java/com/aws/cicd/controller/CourseController.java
+++ tutorials/aws-cicd-codebuild/src/main/java/com/aws/cicd/controller/CourseController.java
@@ -65,15 +65,15 @@ public class CourseController {
         }
     }
 
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "UP";
-    }
+    // @GetMapping("/health")
+    // public String healthCheck() {
+    //     return "UP";
+    // }
 
 
-    @GetMapping("/welcome")
-    public String greetings() {
-        return "Hello Techie , AWS CICD Example working fine !";
-    }
+    // @GetMapping("/welcome")
+    // public String greetings() {
+    //     return "Hello Techie , AWS CICD Example working fine !";
+    // }
 
 }
