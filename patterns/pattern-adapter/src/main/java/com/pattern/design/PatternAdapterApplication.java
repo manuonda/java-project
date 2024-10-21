bb8497aaad5e67bd093f0b07114acba6eae5e3ed
David Garcia <david.garcia@grupomost.com>
2024-10-21 20:33:59 -0300

    Add Patter adapter project

---
+++ patterns/pattern-adapter/src/main/java/com/pattern/design/PatternAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.pattern.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternAdapterApplication.class, args);
+	}
+
+}
