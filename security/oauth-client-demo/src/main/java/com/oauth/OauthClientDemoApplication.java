29b9053720f7daa2ccf85ad95972bc47a2781c6b
david garcia <david.garcia@grupomost.com>
2024-10-29 15:27:06 -0300

    add init project example oauthclient-demo

---
+++ security/oauth-client-demo/src/main/java/com/oauth/OauthClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthClientDemoApplication.class, args);
+	}
+
+}
