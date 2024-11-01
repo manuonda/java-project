cf41fe1a1b7655231468abb78fa622b210769618
david garcia <david.garcia@grupomost.com>
2024-11-01 16:54:17 -0300

    Add Files

--- security/oauth-client-demo/src/main/java/com/oauth/http/controller/Dashboard.java
+++ security/oauth-client-demo/src/main/java/com/oauth/http/controller/Dashboard.java
@@ -2,17 +2,53 @@ package com.oauth.http.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nimbusds.jose.proc.SecurityContext;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @RestController
 @RequestMapping("/api/v1/dashboard")
 public class Dashboard {
 
+    public record UserDTO(String userName, String email, Collection<GrantedAuthority> authorities) {
+
+    }
     @GetMapping
-    public String getDashboardAuthenticated() {
-        return new String("Hello Authenticated");
+    public UserDTO getDashboardAuthenticated(Authentication authentication) {
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+        
+        var userName="";
+        Collection<GrantedAuthority> authorities = null;
+        var email ="";
+        if ( authentication != null && authentication.isAuthenticated()){
+            Object principal = authentication.getPrincipal();
+            if ( principal instanceof UserDetails userDetails){
+                 userName = userDetails.getUsername();
+                 authorities =  new ArrayList<>(userDetails.getAuthorities());
+            } else if ( principal instanceof OAuth2User oAuth2User){
+                userName = oAuth2User.getAttribute("name");
+                email = oAuth2User.getAttribute("email");
+                authorities =  new ArrayList<>(oAuth2User.getAuthorities());
+
+            }
+        }
+
+        return  new UserDTO(userName, email, authorities);
     }
     
 }
