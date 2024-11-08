302a1e5d3badb8c92cde1b5daf5fcf179d675f31
David Garcia <david.garcia@grupomost.com>
2024-11-08 20:42:35 -0300

    Add method request body and postmapping

--- projects/spring-annotation-example/src/main/java/com/spring/annotations/example/controllers/BookController.java
+++ projects/spring-annotation-example/src/main/java/com/spring/annotations/example/controllers/BookController.java
@@ -1,10 +1,14 @@
 package com.spring.annotations.example.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 //@Controller
 //@ResponseBody
@@ -23,6 +27,18 @@ public class BookController {
     @GetMapping(value = {"/books","/java"})
     public Book getBook() {
         return new Book(1, "Title1", "Description one");
+    }
+    
+
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping(value = "/books/create" ,
+    consumes= MediaType.APPLICATION_JSON_VALUE )
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        System.out.println(book.id());
+        System.out.println(book.name());
+        System.out.println(book.description());
+        return ResponseEntity.status(HttpStatus.CREATED).body(book);
+       // return new ResponseEntity<>(book, HttpStatus.CREATED);
     }
     
 
