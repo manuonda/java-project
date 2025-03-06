a4cd88d927e18398bf956464ee1202374649bc48
David Garcia <david.garcia@grupomost.com>
2025-03-06 20:46:16 -0300

    Update Kafka configuration for multithreading tutorial to support additional listeners and modify bootstrap server settings

--- tutorials/multithreading/src/main/java/com/multithreading/entity/Product.java
+++ tutorials/multithreading/src/main/java/com/multithreading/entity/Product.java
@@ -3,6 +3,7 @@ package com.multithreading.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,16 +12,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name="Product")
 public class Product {
     @Id
     private Long id;
     private String name;
     private String category;
     private double price;
-    @Column(name = "isOfferApplied")
+    @Column(name = "is_offer_applied")
     private boolean isOfferApplied;
-    @Column(name = "discountPercentage")
+    @Column(name = "discount_percentage")
     private double discountPercentage;
-    @Column(name = "priceAfterDiscount")
+    @Column(name = "price_after_discount")
     private double priceAfterDiscount;
 }
