b1357b19eba9f60b5b036e2cc29fa1e713b47e82
David Garcia <david.garcia@grupomost.com>
2025-11-05 21:11:46 -0300

    Add Security Utils

---
+++ projects/spring-boot-url-shortener/src/main/java/com/manuonda/urlshortener/web/controller/SecurityUtils.java
@@ -0,0 +1,24 @@
+package com.manuonda.urlshortener.web.controller;
+
+
+import com.manuonda.urlshortener.domain.entities.User;
+import com.manuonda.urlshortener.repositorys.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityUtils {
+
+    private final UserRepository userRepository;
+    public SecurityUtils(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+   public User getCurrentUser(){
+       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+       if(authentication != null && authentication.isAuthenticated()){
+              return this.userRepository.findByEmail(authentication.getName()).orElse(null);
+       }
+       return null;
+   }
+}
